55385b5adcc30e74905f368543f12b68fc6117e7
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-03 22:31:45 +0600

    video title and user image fetched

--- script/video.js
+++ script/video.js
@@ -6,6 +6,9 @@ const loadCatagories = () => {
   .then(data => displayCatagories(data.categories))
   .catch((error)=>console.log(error));
 }
+
+
+
 // create display Category function
 const displayCatagories = (categories) => {
   //add data in html
@@ -22,4 +25,61 @@ const displayCatagories = (categories) => {
 };
 
 
-loadCatagories(); // call loadCatagories function
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+  .then(res => res.json())
+  .then(data => displayVideos(data.videos))
+  .catch(error => console.log(error))
+}
+
+// {
+//   "category_id": "1003",
+//   "video_id": "aaae",
+//   "thumbnail": "https://i.ibb.co/Yc4p5gD/inside-amy.jpg",
+//   "title": "Inside Amy Schumer",
+//   "authors": [
+//       {
+//           "profile_picture": "https://i.ibb.co/YD2mqH7/amy.jpg",
+//           "profile_name": "Amy Schumer",
+//           "verified": ""
+//       }
+//   ],
+//   "others": {
+//       "views": "3.6K",
+//       "posted_date": "15147"
+//   },
+//   "description": "'Inside Amy Schumer' is a comedy show by the popular comedian Amy Schumer, blending sharp satire and unfiltered humor to tackle everyday issues and societal norms. With 3.6K views, the show promises a blend of hilarious sketches, thought-provoking stand-up, and candid interviews. It's a must-watch for fans of bold, edgy comedy."
+// }
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos")
+  videos.forEach(video => {
+    console.log(video)
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+    <figure class="h-48">
+      <img class="h-full w-full object-cover rounded-2xl"
+        src=${video.thumbnail}
+        alt="Shoes" />
+    </figure>
+    <div class="px-0 py-2 flex gap-2">
+      <div>
+        <img class="w-10 h-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
+      </div>
+      <div>
+        <h2 class="font-bold">${video.title}</h2>
+        <div class="flex items-center gap-2">
+          <p>${video.authors[0].profile_name}</p>
+          <img class="h-4" src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png"/>
+        </div>
+        <p></p>
+      </div>
+    </div>
+    `
+    videoContainer.append(card);
+  });
+}
+
+
+loadCatagories();
+loadVideos();
