08982fda81f5d5c65d6e904796ad5d2d88d72da5
Kazi Md. Rakibul Hassan <kazimdrakibulhassan@gmail.com>
2024-10-04 20:37:09 +0600

    verified logo added conditionaly

--- script/video.js
+++ script/video.js
@@ -57,10 +57,11 @@ const displayVideos = (videos) => {
     const card = document.createElement("div");
     card.classList = "card card-compact";
     card.innerHTML = `
-    <figure class="h-48">
+    <figure class="h-52 relative">
       <img class="h-full w-full object-cover rounded-2xl"
         src=${video.thumbnail}
         alt="Shoes" />
+        ${video.others.posted_date === "" ? '' : `<span class="absolute right-2 bottom-2 bg-black rounded p-1 text-gray-400 text-xs">${video.others.posted_date}</span>`}
     </figure>
     <div class="px-0 py-2 flex gap-2">
       <div>
@@ -69,8 +70,8 @@ const displayVideos = (videos) => {
       <div>
         <h2 class="font-bold">${video.title}</h2>
         <div class="flex items-center gap-2">
-          <p>${video.authors[0].profile_name}</p>
-          <img class="h-4" src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png"/>
+          <p class="text-gray-500">${video.authors[0].profile_name}</p>
+          ${video.authors[0].verified === true ? '<img class="h-4" src="https://img.icons8.com/?size=48&id=SRJUuaAShjVD&format=png"/>' : ''}
         </div>
         <p></p>
       </div>
